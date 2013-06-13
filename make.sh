9f5c9d9c3ef0199c09cf3dec04ef779a9cb8d5d7
Mark Seaborn <mseaborn@chromium.org>
2013-06-13 02:49:26 +0000

    new-bc: Remove hard-coded pathname that's specific to my setup
    Require a symlink to be set up instead.

--- make.sh
+++ make.sh
@@ -8,8 +8,18 @@ if which ccache >/dev/null 2>&1; then
   ccache=ccache
 fi
 
-llvm_bin=~/devel/nacl-git3/native_client/toolchain/pnacl_linux_x86/host_x86_64/bin
+llvm_bin=toolchain/pnacl_linux_x86/host_x86_64/bin
 llvm_config=$llvm_bin/llvm-config
+
+if [ ! -e toolchain ]; then
+  echo "Error: Expected to find the native_client/toolchain directory symlinked here"
+  echo "Try: ln -s .../native_client/toolchain"
+  exit 1
+fi
+if [ ! -e "$llvm_config" ]; then
+  echo "Error: \"$llvm_config\" is not present"
+  exit 1
+fi
 
 mkdir -p out
 
